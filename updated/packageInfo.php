d81ed2bb12b7dcfea80495f25a60fa24ae7cbada
chanthri383 <chanthri123@gmail.com>
2020-04-19 21:08:07 -0500

    Add files via upload

--- updated/packageInfo.php
+++ updated/packageInfo.php
@@ -57,7 +57,7 @@
                         <option value="Colorado">Colorado</option>
                         <option value="Connecticut">Connecticut</option>
                         <option value="Delaware">Delaware</option>
-                        <option value="Floria">Floria</option>
+                        <option value="Florida">Florida</option>
                         <option value="Georgia">Georgia</option>
                         <option value="Hawaii">Hawaii</option>
                         <option value="Idaho">Idaho</option>
